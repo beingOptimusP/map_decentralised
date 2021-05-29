0c7505380ee81588b88f7234ba34eba2b2b7b48c
vamsi <75676297+beingOptimusP@users.noreply.github.com>
2021-05-29 16:20:46 +0530

    added transfercmap function

--- contracts/map.sol
+++ contracts/map.sol
@@ -1,11 +1,12 @@
+//SPDX-License-Identifier: MIT;
+
 pragma solidity >=0.4.22 <0.7.0;
 
 // import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MAP {
-    
     //ERC20
-    
+
     // Public variables of the token
     string public name;
     string public symbol;
@@ -14,14 +15,18 @@ contract MAP {
     uint256 public totalSupply;
 
     // This creates an array with all balances
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     // This generates a public event on the blockchain that will notify clients
     event Transfer(address indexed from, address indexed to, uint256 value);
-    
+
     // This generates a public event on the blockchain that will notify clients
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
     // This notifies clients about the amount burnt
     event Burn(address indexed from, uint256 value);
@@ -33,17 +38,20 @@ contract MAP {
      */
     constructor() public {
         decimals = 18;
-        totalSupply = 1000000 * 10 ** uint256(decimals);  // Update total supply with the decimal amount
-        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
-        name = "MAP";                                   // Set the name for display purposes
-        symbol = "MAP";                               // Set the symbol for display purposes
-        timeStamp = now;
+        totalSupply = 1000000 * 10**uint256(decimals); // Update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply; // Give the creator all initial tokens
+        name = "MAP"; // Set the name for display purposes
+        symbol = "MAP";
     }
 
     /**
      * Internal transfer, only can be called by this contract
      */
-    function _transfer(address _from, address _to, uint _value) internal {
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) internal {
         // Prevent transfer to 0x0 address. Use burn() instead
         require(_to != address(0x0));
         // Check if the sender has enough
@@ -51,7 +59,7 @@ contract MAP {
         // Check for overflows
         require(balanceOf[_to] + _value >= balanceOf[_to]);
         // Save this for an assertion in the future
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        uint256 previousBalances = balanceOf[_from] + balanceOf[_to];
         // Subtract from the sender
         balanceOf[_from] -= _value;
         // Add the same to the recipient
@@ -69,7 +77,10 @@ contract MAP {
      * @param _to The address of the recipient
      * @param _value the amount to send
      */
-    function transfer(address _to, uint256 _value) public returns (bool success) {
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
         _transfer(msg.sender, _to, _value);
         return true;
     }
@@ -83,8 +94,12 @@ contract MAP {
      * @param _to The address of the recipient
      * @param _value the amount to send
      */
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]); // Check allowance
         allowance[_from][msg.sender] -= _value;
         _transfer(_from, _to, _value);
         return true;
@@ -98,60 +113,119 @@ contract MAP {
      * @param _spender The address authorized to spend
      * @param _value the max amount they can spend
      */
-    function approve(address _spender, uint256 _value) public
-        returns (bool success) {
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
-    
+
     //BANK and ERC721
 
-    mapping (address => uint256) public balanceBank;
-    mapping (uint => address) public token;
-    mapping (address => uint) public tokenNo;
-    mapping (uint => TokenData) public data;
-    // mapping (uint => uint[]) ;
-    
-    uint private cmap;
-    uint private tokenInBank;
-    uint private inflation;
-    uint timeStamp;
-    uint[] private tokens; 
-    struct TokenData{
-        uint tokenId;
-        uint time;
-        uint holdings;
-        uint interest;
+    // mapping (address => uint256) public balanceBank;
+
+    //mapping for owner of token
+    mapping(uint256 => address) public token;
+
+    //number of token an address holds
+    mapping(address => uint256) public tokenNo;
+
+    //token data is is mapped with token id
+    mapping(uint256 => TokenData) public data;
+
+    //tokenIDs
+    uint256 private cmap;
+
+    //total tokens deposited into bank
+    uint256 private tokenInBank;
+
+    //inflation percentage
+    uint256 private inflation;
+
+    //token data
+    struct TokenData {
+        uint256 tokenId;
+        uint256 time;
+        uint256 holdings;
+        uint256 interest;
     }
 
-    function deposit(uint _value) public{
-        require(_value <= balanceOf[msg.sender],"no enough funds in ur wallet");
-        balanceBank[msg.sender] += _value; 
+    function deposit(uint256 _value) public {
+        require(
+            _value <= balanceOf[msg.sender],
+            "no enough funds in ur wallet"
+        );
+
+        //subs tokens from user wallet
         balanceOf[msg.sender] -= _value;
-        tokenInBank += _value;
-        inflation = ((totalSupply - tokenInBank)*100*(now - timeStamp))/( totalSupply*31536000 );
-        timeStamp = now;
-        token[cmap] = msg.sender;
-        data[cmap].tokenId = cmap;
-        data[cmap].time = now;
+
+        //adds tokens to cmap holdings
         data[cmap].holdings = _value;
+
+        //adds tokens to total bank holdings
+        tokenInBank += _value;
+
+        //finds interest %
+        inflation =
+            ((totalSupply - tokenInBank) * 100 * 1000000) /
+            (totalSupply);
+
+        //maps cmap token to the user who deposited tokens
+        token[cmap] = msg.sender;
+
+        //stores tokenId
+        data[cmap].tokenId = cmap;
+
+        //storing time of the deposit
+        data[cmap].time = now;
+
+        //storing interest rate at that moment
         data[cmap].interest = inflation;
+
+        //incrementing tokenid
         cmap++;
+
+        //incrementing number of tokens held by the depositor
         tokenNo[msg.sender]++;
     }
 
-    function withdraw(uint _tokenId) public{
-        require(token[_tokenId] == msg.sender,"this token dont belongs to you");
-        balanceBank[msg.sender] -= data[_tokenId].holdings; 
-        balanceOf[msg.sender] += data[_tokenId].holdings;
+    function withdraw(uint256 _tokenId) public {
+        require(
+            token[_tokenId] == msg.sender,
+            "this token dont belongs to you"
+        );
+
+        //updating total tokens in bank
         tokenInBank -= data[_tokenId].holdings;
-        uint interestAmount = balanceBank[msg.sender]*data[_tokenId].interest/100;
-        balanceOf[msg.sender] += (data[_tokenId].holdings+interestAmount);
-        balanceBank[msg.sender] -= data[_tokenId].holdings;
-        
+
+        //calculating interest amount;
+        uint256 interestAmount =
+            ((data[_tokenId].holdings * data[_tokenId].interest) *
+                (now - data[_tokenId].time)) / (100 * 31536000 * 1000000);
+
+        //transfering tokens to users wallet
+        balanceOf[msg.sender] += (data[_tokenId].holdings + interestAmount);
+
+        //totalSupply Update
+        totalSupply += interestAmount;
+
+        //buring cmap token
         token[_tokenId] = address(0x0);
+
+        //updating number of cmap tokens held by the user
         tokenNo[msg.sender]--;
     }
-    
+
+    function transferCmap(address _id, uint256 _tokenId) public {
+        require(token[_tokenId] == msg.sender, "This token dont belong to you");
+
+        //token tranfered to said address
+        token[_tokenId] = _id;
+
+        //updating token number for both addresses
+        tokenNo[msg.sender]--;
+        tokenNo[_id]++;
+    }
 }
